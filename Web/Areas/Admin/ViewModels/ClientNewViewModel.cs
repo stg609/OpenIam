02c5a4b222444393c41c8e89f4da96695fd3b9b9
Charlie <stg609@outlook.com>
2021-01-26 13:53:24 +0800

    添加客户端的时候自动把 silent-renew 也作为 redirectUri 添加

--- Web/Areas/Admin/ViewModels/ClientNewViewModel.cs
+++ Web/Areas/Admin/ViewModels/ClientNewViewModel.cs
@@ -36,7 +36,7 @@ namespace Charlie.OpenIam.Web.Areas.Admin.ViewModels
         public string ClientUri { get; set; }
 
         /// <summary>
-        /// 回调地址，多个地址用英文逗号分隔
+        /// 回调地址，多个地址用英文逗号分隔。这些地址主要包括用于完成 OAuth 流程的 signin-oidc 以及 用于 SPA 静默更新的 silent-renew
         /// </summary>
         public string RedirectUris { get; set; }
 
